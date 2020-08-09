84d5541b9abb1b74f39738dc8d40d42b9437c9e4
nix-chu <nmchua8@gmail.com>
2020-08-10 00:21:14 +1000

    Rename Task builder to Builder

--- app/src/main/java/com/nix_chu/organisingapp/Task.java
+++ app/src/main/java/com/nix_chu/organisingapp/Task.java
@@ -8,41 +8,41 @@ public class Task {
     private final String description;
     private final String location;
 
-    private Task(TaskBuilder builder) {
+    private Task(Builder builder) {
         this.name = builder.name;
         this.date = builder.date;
         this.description = builder.description;
         this.location = builder.location;
     }
 
-    public String getName() { return name; }
-    public Date getDate() { return date; }
-    public String getDescription() { return description; }
-    public String getLocation() { return location; }
+    public String getName() { return this.name; }
+    public Date getDate() { return this.date; }
+    public String getDescription() { return this.description; }
+    public String getLocation() { return this.location; }
 
-    public static class TaskBuilder {
+    public static class Builder {
         // Creates an instance of Task which takes optional arguments
         private String name; // required
         private Date date;
         private String description;
         private String location;
 
-        public TaskBuilder(String name) {
+        public Builder(String name) {
             // All required attributes
             this.name = name;
         }
 
-        public TaskBuilder date(final int date) {
+        public Builder date(final int date) {
             this.date = new Date();
             return this;
         }
 
-        public TaskBuilder description(final String description) {
+        public Builder description(final String description) {
             this.description = description;
             return this;
         }
 
-        public TaskBuilder location(final String location) {
+        public Builder location(final String location) {
             this.location = location;
             return location;
         }
