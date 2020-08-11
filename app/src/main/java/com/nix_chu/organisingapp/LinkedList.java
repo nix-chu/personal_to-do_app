73d6ba23c6bdceccf383badeda94d95190ab26fe
nix-chu <nmchua8@gmail.com>
2020-08-12 00:33:14 +1000

    Create methods for LinkedList

--- app/src/main/java/com/nix_chu/organisingapp/LinkedList.java
+++ app/src/main/java/com/nix_chu/organisingapp/LinkedList.java
@@ -10,14 +10,19 @@ public class LinkedList {
 
     private void setHead(Node node) { this.head = node; } // Only use when inserting an item at head
 
+    private boolean isEmpty() {
+        // Return boolean if list is empty
+        return this.length == 0;
+    }
+
     public void insert(int index, Task task) {
-        // Insert an item (the task) at the index specified
+        // Insert task at the index specified
         final Node newNode = new Node(task);
         if (index == 0) {
             newNode.setLink(this.head);
             this.setHead(newNode);
         }
-        elif (index == this.length) {
+        else if (index == this.length) {
             final Node previousNode = this.findNode(index);
             previousNode.setLink(newNode);
         }
@@ -29,9 +34,45 @@ public class LinkedList {
         this.length++;
     }
 
-    public Node findNode(int index) {
+    public void append(Task task) {
+        // Adds the given task at the end of the list
+        this.insert(this.length, task);
+    }
+
+    public void delete(int index) {
+        // Delete task at index
+        if (this.isEmpty()) {
+            throw new NullPointerException("List is empty.");
+        }
+        else if (index == 0) {
+            this.head = this.head.getLink();
+        }
+        else {
+            Node previousNode = this.findNode(index - 1);
+            Node nextNode = previousNode.getLink().getLink();
+            previousNode.setLink(nextNode);
+        }
+        this.length--;
+    }
+    public void clear() {
+        // Clear all tasks
+        this.length = 0;
+        this.head = null;
+    }
+
+    public Task getTask(int index) {
+        // Return the task at index
+        // TODO: Perhaps change this to the task name given
+        if (this.isEmpty()) {
+            throw new NullPointerException("List is empty.");
+        }
+        return this.findNode(index).getItem();
+    }
+
+    private Node findNode(int index) {
+        // Find the task at index provided and return
         if (index < 0 || index > this.length) {
-            throw new ArrayIndexOutOfBoundsException();
+            throw new ArrayIndexOutOfBoundsException("Index is out of bounds.");
         }
         else {
             Node currentNode = this.head;
